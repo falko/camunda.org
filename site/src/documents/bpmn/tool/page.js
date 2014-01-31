d518bbe182e7695e0490dc2ba98bf123750584fa
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2014-01-31 16:08:45 +0100

    feat(camunda-modeler): improve SEO and accessibility
    - moved modeler page to /bpmn/tool
    - add modeler screenshots as separate images
    - add google analytics linking

--- site/src/documents/bpmn/tool/page.js
+++ site/src/documents/bpmn/tool/page.js
@@ -1,32 +1,4 @@
 /**
- * index page script
- */
-
-(function($) {
-  $(document).ready(function() {
-
-    var jumbo, description, nav;
-
-    jumbo = $('.col-jumbo');
-    nav = jumbo.find('.nav');
-    description = jumbo.find('.description');
-
-    nav.find('[data-slide]').each(function() {
-      var e = $(this);
-
-      e.click(function() {
-        nav.find('li').removeClass('active');
-        e.parent().addClass('active');
-
-        jumbo.removeClass('slide-1 slide-2 slide-3 slide-4');
-        jumbo.addClass(e.data('slide'));
-      });
-    });
-  });
-
-})(window.jQuery);
-
-/**
  * Download page
  */
 (function(window, $, release) {
